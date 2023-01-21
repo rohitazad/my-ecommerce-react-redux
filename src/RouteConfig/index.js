15b92a5e793b077aa41ba303eb85c956dfb1d049
rohitazad <ram9953878727@gmail.com>
2023-01-21 16:38:41 +0530

    code splitting

--- src/RouteConfig/index.js
+++ src/RouteConfig/index.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 import HeaderComponents from '../Components/Header';
-import HomePageContainer from '../Containers/Home';
-import ContactPageContainer from '../Containers/Contact';
-import AboutPageContainer from '../Containers/About';
-import CartPageContainer from '../Containers/Cart';
-import ProductsPageContainer from '../Containers/Products';
-import SingleProductContainer from '../Containers/SingleProduct';
-import SearchContainerPage from '../Containers/Search';
-import LoginContainer from '../Containers/Login';
 import FooterComponent from '../Components/Footer';
-import UserContainer from '../Containers/User';
+const HomePageContainer =  lazy(()=> import ('../Containers/Home')) ;
+const ContactPageContainer =  lazy(()=> import ('../Containers/Contact')) ;
+const AboutPageContainer = lazy(()=> import ('../Containers/About')) ;
+const CartPageContainer = lazy (()=> import ('../Containers/Cart')) ;
+const ProductsPageContainer = lazy (()=> import ('../Containers/Products'));
+const SingleProductContainer = lazy (()=> import ( '../Containers/SingleProduct'));
+const SearchContainerPage = lazy (()=> import ( '../Containers/Search'));
+const LoginContainer = lazy (()=> import ( '../Containers/Login'));
+const UserContainer = lazy (()=> import ( '../Containers/User'));
 const RouteConfigComponents = ()=>{
     return (
         <>
             <BrowserRouter>
                 <HeaderComponents />
-                <Routes>
-                    <Route path="/" element={<HomePageContainer />} />
-                    <Route path="/about" element={<AboutPageContainer />} />
-                    <Route path="/contact" element={<ContactPageContainer />} />
-                    <Route path="/cart" element={<CartPageContainer />} />
-                    <Route path="/product/:id" element={<SingleProductContainer />} />
-                    <Route path="/products/:catId" element={<ProductsPageContainer />} />
-                    <Route path="/search/:searchKey" element={<SearchContainerPage />} />
-                    <Route path="/login" element={<LoginContainer />} />
-                    <Route path='/user' element={<UserContainer />} />
-                </Routes>
+                <Suspense fallback={<div>Loading... please wait</div>}>
+                    <Routes>
+                        <Route path="/" element={<HomePageContainer />} />
+                        <Route path="/about" element={<AboutPageContainer />} />
+                        <Route path="/contact" element={<ContactPageContainer />} />
+                        <Route path="/cart" element={<CartPageContainer />} />
+                        <Route path="/product/:id" element={<SingleProductContainer />} />
+                        <Route path="/products/:catId" element={<ProductsPageContainer />} />
+                        <Route path="/search/:searchKey" element={<SearchContainerPage />} />
+                        <Route path="/login" element={<LoginContainer />} />
+                        <Route path='/user' element={<UserContainer />} />
+                    </Routes>
+                </Suspense>
                 <FooterComponent />
             </BrowserRouter>
         </>
